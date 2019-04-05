74572c66b6969243dc83e868b0e8509645bb04e2
Thiago Brandão Damasceno <thiagobrandam@gmail.com>
2019-04-05 09:27:37 -0300

    Allow for script’ed injection of json in html

--- src/config/dom-listener.js
+++ src/config/dom-listener.js
@@ -2,10 +2,29 @@ import Vue from 'vue/dist/vue.js'
 
 const Mounter = () => {
   document.addEventListener('DOMContentLoaded', () => {
-    var nodes = document.querySelectorAll('[data-vue-cpt]')
+    let nodes = document.querySelectorAll('[data-vue-cpt]')
 
-    for(var i = 0; i < nodes.length; ++i) {
-      new Vue({el: nodes[i], template: document.getElementById(nodes[i].getAttribute('data-vue-cpt')).innerHTML})
+    for(let i = 0; i < nodes.length; ++i) {
+      let node      = nodes[i]
+      let data      = {}
+      let cptName   = node.attributes['data-vue-cpt'].value
+      let vueConfig = {
+        el: node,
+        template: document.getElementById(node.getAttribute('data-vue-cpt')).innerHTML
+      }
+
+      let props = document.querySelectorAll(`script[for="${cptName}"]`)
+
+      for (let j = 0; j < props.length; j++){
+        let propNode   = props[j]
+        let propName   = propNode.attributes['name'].value
+        data[propName] = JSON.parse(propNode.textContent)
+      }
+
+      if (Object.keys(data).length) {
+        vueConfig.data = data
+      }
+      new Vue(vueConfig)
     }
 
   });
